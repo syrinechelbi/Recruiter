63e4934d6ab9f60492579d47743f4e48e362df61
shelbisourour <shelbisourour@gmail.com>
2023-08-03 12:14:50 +0200

    fix big

--- src/registerC.js
+++ src/registerC.js
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-
-import "./login.css";
 import { Button, Card, Container, Form } from "react-bootstrap";
 import { Navigate } from "react-router-dom";
 import axios from "axios";
@@ -16,8 +14,9 @@ function AppregisterC() {
   });
 
   const handleChange = ({ currentTarget: Input }) => {
-    setData({ data, [Input.name]: Input.value });
+    setData({ ...data, [Input.name]: Input.value });
   };
+
   const [error, setError] = useState("");
 
   const handleSubmit = async (e) => {
@@ -38,93 +37,86 @@ function AppregisterC() {
       }
     }
   };
+
   const [selectedOption, setSelectedOption] = useState("option1");
 
   const handleRadioChange = (event) => {
     setSelectedOption(event.target.value);
-
-    return (
-      <Container style={{ width: "30rem", height: "25rem" }}>
-        <Card style={{ width: "30rem" }}>
-          <Card.Img variant="center" src="./sopra.png" className="card-img" />
-          <h5 className="fw-normal my-4 pb-3" style={{ letterSpacing: "1px" }}>
-            Register as a Candidate
-          </h5>
-          <Card.Body>
-            <form onSubmit={handleSubmit}>
-              <Form.Label>Firstname</Form.Label>
-              <Form.Control
-                name="firstName"
-                type="text"
-                value={data.firstName}
-                required
-                onChange={handleChange}
-              />
-              <Form.Label>Lastname</Form.Label>
-              <Form.Control
-                type="text"
-                name="lastName"
-                value={data.lastName}
-                required
-                onChange={handleChange}
-              />
-              <Form.Label>phone</Form.Label>
-              <Form.Control
-                name="phone"
-                type="text"
-                value={data.phone}
-                onChange={handleChange}
-              />
-              <Form.Label>Gender</Form.Label>
-              <Form.Check
-                value="female"
-                type="radio"
-                aria-label="radio 1"
-                label="female"
-                checked={selectedOption === "f"}
-                onChange={handleRadioChange}
-              />
-
-              <Form.Check
-                value="male"
-                type="radio"
-                aria-label="radio 2"
-                label="male"
-                checked={selectedOption === "m"}
-                onChange={handleRadioChange}
-              />
-
-              <Form.Label>email</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Email"
-                value={data.email}
-                onChange={handleChange}
-              />
-
-              <Form.Label htmlFor="inputPassword5">Password</Form.Label>
-              <Form.Control
-                type="password"
-                id="inputPassword5"
-                aria-describedby="passwordHelpBlock"
-                value={data.password}
-                onChange={handleChange}
-              />
-              {error && <div className={StyleSheet.error_msg}>error</div>}
-              <Button variant="dark" className="bouton" type="submit">
-                Register
-              </Button>
-            </form>
-          </Card.Body>
-          <br />
-          <br />
-
-          <br />
-          <br />
-        </Card>
-      </Container>
-    );
   };
+
+  return (
+    <Container style={{ width: "30rem", height: "25rem" }}>
+      <Card style={{ width: "30rem" }}>
+        <Card.Img variant="center" src="./sopra.png" className="card-img" />
+        <h5 className="fw-normal my-4 pb-3" style={{ letterSpacing: "1px" }}>
+          Register as a Candidate
+        </h5>
+        <Card.Body>
+          <form onSubmit={handleSubmit}>
+            <Form.Label>Firstname</Form.Label>
+            <Form.Control
+              name="firstName"
+              type="text"
+              value={data.firstName}
+              required
+              onChange={handleChange}
+            />
+            <Form.Label>Lastname</Form.Label>
+            <Form.Control
+              type="text"
+              name="lastName"
+              value={data.lastName}
+              required
+              onChange={handleChange}
+            />
+            <Form.Label>phone</Form.Label>
+            <Form.Control
+              name="phone"
+              type="text"
+              value={data.phone}
+              onChange={handleChange}
+            />
+            <Form.Label>Gender</Form.Label>
+            <Form.Check
+              value="female"
+              type="radio"
+              aria-label="radio 1"
+              label="female"
+              checked={selectedOption === "female"}
+              onChange={handleRadioChange}
+            />
+            <Form.Check
+              value="male"
+              type="radio"
+              aria-label="radio 2"
+              label="male"
+              checked={selectedOption === "male"}
+              onChange={handleRadioChange}
+            />
+            <Form.Label>email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Email"
+              value={data.email}
+              onChange={handleChange}
+            />
+            <Form.Label htmlFor="inputPassword5">Password</Form.Label>
+            <Form.Control
+              type="password"
+              id="inputPassword5"
+              aria-describedby="passwordHelpBlock"
+              value={data.password}
+              onChange={handleChange}
+            />
+            {error && <div className={StyleSheet.error_msg}>{error}</div>}
+            <Button variant="dark" className="bouton" type="submit">
+              Register
+            </Button>
+          </form>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
 }
 
 export default AppregisterC;
